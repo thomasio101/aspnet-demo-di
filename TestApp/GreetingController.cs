c402bd07a1a282889b7401bf6b4e2d58c85b6b3c
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-05-18 11:16:58 +0200

    Add project files.

---
+++ TestApp/GreetingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp
+{
+    [Route("/")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        private readonly IGreetingService _greetingService;
+
+        public GreetingController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        public string Index([FromQuery]string name)
+        {
+            return _greetingService.Greet(name);
+        }
+    }
+}
---
+++ TestApp/GreetingService.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApp
+{
+    public interface IGreetingService
+    {
+        string Greet(string name);
+    }
+
+    public class GreetingService : IGreetingService
+    {
+        public string Greet(string name) => $"Hello, {name}!";
+    }
+}
